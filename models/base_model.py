f399c95314bd37ffb6d49b20f7eca95508a9ac65
Dannie55 <ezirimdaniel080@gmail.com>
2023-05-08 01:51:12 -0700

    created the base model class for the AirBnB project

---
+++ models/base_model.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Defines the BaseModel Class"""
+import models
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """Represent a class from which other classes inherits""".
+
+    def __init__(self, *args, **kwargs):
+
+        """initializes a new BaseModel,
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict) : key/value pairs of attributes
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == 'created_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    if 'id' not in kwargs.keys():
+                        self.id = str(uuid4())
+                    if 'created_at' not in kwargs.keys():
+                        self.created_at = datetime.now()
+                    if 'updated_at' not in kwargs.keys():
+                        self.updated_at = datetime.now()
+                setattr(self, key, value)
+           
+        else:
+                self.id = str(uuid4()))
+                self.created_at = datetime.now()
+                self.updated_at = self.created_at
+                models.storage.new(self)
+    
+    def __str__(self):
+        """string"""
+        return ('[' + type(self).__name__ + '] (' + str(self.id) +
+                ') ' + str(self.__dict__))
+    
+    def save(self):
+        """Save Function"""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """Return a dictionary"""
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"]
+        my_dict["updated_at"] = my_dict["updated_at"]
+        return my_dict
