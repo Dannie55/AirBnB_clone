f98d6dbbe50aa2cf9a02bade9367e1864b679e6d
Dannie55 <ezirimdaniel080@gmail.com>
2023-05-08 05:34:16 -0700

    Base model class that other class inherits

--- models/base_model.py
+++ models/base_model.py
@@ -24,34 +24,33 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == 'created_at':
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    if 'id' not in kwargs.keys():
-                        self.id = str(uuid4())
-                    if 'created_at' not in kwargs.keys():
-                        self.created_at = datetime.now()
-                    if 'updated_at' not in kwargs.keys():
-                        self.updated_at = datetime.now()
+                if 'id' not in kwargs.keys():
+                    self.id = str(uuid4())
+                if 'created_at' not in kwargs.keys():
+                    self.created_at = datetime.now()
+                if 'updated_at' not in kwargs.keys():
+                    self.updated_at = datetime.now()
                 setattr(self, key, value)
-           
         else:
-                self.id = str(uuid4()))
-                self.created_at = datetime.now()
-                self.updated_at = self.created_at
-                models.storage.new(self)
-    
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
+
     def __str__(self):
-        """string"""
-        return ('[' + type(self).__name__ + '] (' + str(self.id) +
-                ') ' + str(self.__dict__))
-    
+        """String"""
+        return('[' + type(self).__name__ + '] (' + str(self.id) +
+               ') ' + str(self.__dict__))
+
     def save(self):
-        """Save Function"""
+        """save function"""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
         """Return a dictionary"""
         my_dict = self.__dict__.copy()
-        my_dict["__class__"] = type(self).__name__
-        my_dict["created_at"] = my_dict["created_at"]
-        my_dict["updated_at"] = my_dict["updated_at"]
+        my_dict['__class__'] = self.__class__.__name__
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
         return my_dict
